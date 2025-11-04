70f962d9bc1de098ff228037b7638704916f7daa
DennisApaydin <Dennis.Apaydin@edu.fhdw.de>
2025-11-04 09:34:28 +0100

    ImmutableSet

--- src/code/ImmutableSet.java
+++ src/code/ImmutableSet.java
@@ -2,4 +2,25 @@ package code;
 
 public class ImmutableSet {
 
+	private final Node root;
+	
+	private static class Node {
+		public final int value;
+		public final Node next;
+
+		Node(int value, Node next) {
+			this.value = value;
+			this.next = next;
+		}
+	}	
+	
+	
+	public ImmutableSet(Node root) {
+		this.root = root;
+	}
+	
+	public ImmutableSet add(int value) {	
+		return new ImmutableSet(new Node(value, root));
+	}
+	
 }
